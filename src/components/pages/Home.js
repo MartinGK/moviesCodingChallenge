601576882d2d092b8568e0bba0f5b7e7c9c3c6dd
MartinGK <MartinGK-PC>
2020-07-15 21:44:06 -0300

    grid added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,22 +14,27 @@ export default function Home() {
 
     return (
         <>
-        <div className="backgroundImage" />
-        <div className="homeComponent">
-            <nav className="topNavComponent">
-                <h1 className="pageTitle">Your Local Movie Theater!</h1>
-                <SearchInput style={{marginRight:"2rem"}}/>
-            </nav>
-            <h2 className="centerTitle">Recomendations</h2>
-            <section className="movieListContainer">
-                {movies.map((movie) =>
-                    <article key={`movie-${movie.title}`}>
-                        <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={`${movie.title}-poster`} />
-                        {movie.title} - {movie.vote_average}
+            <div className="backgroundImage" />
+            <div className="topNavbarContainer">
+                <nav className="topNavComponent">
+                    <h1 className="pageTitle">Your Local Movie Theater!</h1>
+                    <SearchInput style={{ marginRight: "2rem" }} />
+                </nav>
+            </div>
+            <div className="homeComponent">
+                <h2 className="centerTitle sectionTitle">Recomendations</h2>
+                <section className="movieListContainer">
+                    {movies.sort((a, b) => b.vote_average - a.vote_average)
+                        .map((movie, i) =>
+                        <article className={i < 3 ? `itemArticle--popular-${i}`: "itemArticle"} key={`movie-${movie.title}`}>
+                            <img className="itemArticle--img" src={`https://image.tmdb.org/t/p/w${i<3?"500":"200"}/${movie.poster_path}`} alt={`${movie.title}-poster`} />
+                        </article>
+                    )}
+                    <article className="itemArticle--viewMore">
+                        View more ->
                     </article>
-                )}
-            </section>
-        </div>
+                </section>
+            </div>
         </>
     )
 }
