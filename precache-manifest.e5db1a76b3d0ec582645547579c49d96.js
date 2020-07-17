ccb243692eacfea499c176099eea9f5ec08338ff
MartinGK <MartinGK-PC>
2020-07-16 21:32:03 -0300

    Updates

---
+++ precache-manifest.e5db1a76b3d0ec582645547579c49d96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8efb87fb441e7a1a24d3652c6a6023e7",
+    "url": "/moviesCodingChallenge/index.html"
+  },
+  {
+    "revision": "a6889387713f3df261a2",
+    "url": "/moviesCodingChallenge/static/css/main.cd033786.chunk.css"
+  },
+  {
+    "revision": "f742732b0112222a5a1f",
+    "url": "/moviesCodingChallenge/static/js/2.51dd2559.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moviesCodingChallenge/static/js/2.51dd2559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6889387713f3df261a2",
+    "url": "/moviesCodingChallenge/static/js/main.e350d2ea.chunk.js"
+  },
+  {
+    "revision": "3e7331890faf4daa2390",
+    "url": "/moviesCodingChallenge/static/js/runtime-main.f112f644.js"
+  }
+]);
